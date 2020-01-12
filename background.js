ced5d0a057a9fa190944dc0c508df976b0b123a3
Ryan Oh <ohryan55@gmail.com>
2020-01-11 22:30:59 -0800

    Removing onPageChanged rule

--- background.js
+++ background.js
@@ -8,12 +8,4 @@ chrome.runtime.onInstalled.addListener(function() {
   chrome.storage.sync.set({toggle: true}, function() {
     console.log('Extension running!');
   });
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        // pageUrl: {hostEquals: 'developer.chrome.com'},
-      })],
-      actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
-  });
 });
